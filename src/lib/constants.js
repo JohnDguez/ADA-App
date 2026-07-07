da53907bb35294c512ea47ea9f8cde86e80873fd
Johnatan Dominguez <me.johnatan@gmail.com>
2026-07-06 23:22:21 -0700

    Add files via upload

---
+++ src/lib/constants.js
@@ -0,0 +1,3 @@
+// Fuente única del nombre de la app en toda la UI (títulos, alt text, textos legales, etc.)
+// Si vuelve a cambiar el nombre de marca, solo se edita aquí.
+export const APP_NAME = 'LunaPay'
